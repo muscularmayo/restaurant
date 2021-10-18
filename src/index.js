cfae492886616086175bafcfd2a38866f9dae263
Chris Risden <christopherrisden@gmail.com>
2021-10-17 20:28:45 -0700

    a home page is created, index.js needs to be refactored

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
+import './style.css';
+import Pizzeria from './pizza.jpeg'
 
-function title () {
-  const element = document.createElement('h1')
+function onLoad () {
+  const element = document.createElement('div')
+  element.classList.add('innerContent')
 
-  element.innerHTML = "PADDY'S PUB"
+  const header = document.createElement('header')
+  const title = document.createElement('h1')
+  title.innerHTML = "PADDY'S PUB"
+  title.classList.add('title')
+  header.appendChild(title)
 
+  //add the image to content
+
+  const pizza = new Image();
+  pizza.src = Pizzeria;
+  pizza.classList.add('pizza')
+
+  const copy = document.createElement('p');
+  copy.innerHTML = `Whattup!
+  We're three cool guys looking for other cool guys who wanna hang out in our party mansion.
+  NOTHING SEXUAL.
+  Dudes in good shape encouraged.
+  If you're fat, you should be able to find humour in the little things.
+  Again, nothing sexual.`
+  copy.classList.add('copy')
+
+  element.appendChild(header);
+  element.appendChild(pizza);
+  element.appendChild(copy);
   return element;
 }
 const content = document.querySelector('#content')
-content.appendChild(title())
+content.appendChild(onLoad())
+
+function clearContent() {
+  while (content.firstChild) {
+    content.removeChild(parent.firstChild);
+  }
+}
+
+
+
